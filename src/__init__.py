f98251793533a89718618e11cf1fcc16817230c9
smau8 <sma2244@columbia.edu>
2021-03-08 00:46:09 -0800

    update init

--- src/__init__.py
+++ src/__init__.py
@@ -6,4 +6,5 @@ init file for distmetric package
 
 __version__ = "0.0.1"
 
-from dismetric.src import quartets
+from distmetric.quartets import quartets
+from distmetric.sampling import generate_randomtrees, pairwise_sampling, random_sampling
